0292acc7872be1d8be90769a64e57bd91042421a
VisovanC <catalin.visovan03@e-uvt.ro>
2024-05-22 01:33:18 +0300

    errors and generating

--- build/main.c
+++ build/main.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <openssl/aes.h>
+#include <openssl/rand.h>
+
+#define KEY_SIZE 16
+unsigned char key[KEY_SIZE];
+unsigned char iv[AES_BLOCK_SIZE];
+
+void errors(){
+	perror("Error!");
+	exit(EXIT_FAILURE);
+}
+
+void KeyGen(){
+	if(!RAND_bytes(key, sizeof(key))) errors();
+	if(!RAND_bytes(iv, sizeof(key))) errors();
+}
+
+
 
 int main(){
 
